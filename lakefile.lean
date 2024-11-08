75a7e7b73d2e920e213989641782e1d9f747f052
Peter <pcr3w.priv@gmail.com>
2024-11-08 12:46:22 +0000

    Change toolkit again and reorder lakefile

--- lakefile.lean
+++ lakefile.lean
@@ -10,13 +10,13 @@ package «Integration» where
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
-
-@[default_target]
-lean_lib «Integration» where
-  -- add any library configuration options here
-
+  
 require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
 
 meta if get_config? env = some "dev" then
 require «doc-gen4» from git
   "https://github.com/leanprover/doc-gen4" @ "main"
+
+@[default_target]
+lean_lib «Integration» where
+  -- add any library configuration options here
